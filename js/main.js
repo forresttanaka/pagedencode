fed671f79b80447cddfabc4f3e7ab58e83ae0edd
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-08-26 14:06:57 -0700

    Getting data from test.encodedcc.org
    Displaying in a React component

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
+import React from 'react';
+import {render} from 'react-dom';
 import 'whatwg-fetch';
 
-fetch('https://www.encodeproject.org/search/?type=Experiment&format=json')
-.then(response => {
-    return response.text();
-}).then(body => {
-    console.log(body);
-});
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
+        this.getSegment().then((data) => {
+            this.setState({data: data});
+        });
+    }
+
+    getSegment(start) {
+        return fetch('https://test.encodedcc.org/search/?type=Experiment&format=json')
+        .then(response => {
+            return response.text();
+        }).then(body => {
+            var data = JSON.parse(body);
+            return Promise.resolve(data['@graph'].map(result => result.accession));
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello there</p>
+                {this.state.data.join(', ')}
+            </div>
+        );
+    }
+};
+
+
+render(<App />, document.getElementById('app'));
