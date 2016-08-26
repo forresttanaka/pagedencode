c11d25f1ef44b050ac9a35ffdbb098aef9e2ac7c
Forrest Tanaka <forrest@forrest-tanaka.com>
2016-08-26 14:46:09 -0700

    Retrieve six 25-entry data

--- js/main.js
+++ js/main.js
@@ -7,19 +7,44 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {data: []};
-        this.getSegment().then((data) => {
+        this.start = 0;
+        this.max = 5;
+
+        // GET initial ENCODE data
+        this.getSegment(this.start).then((data) => {
             this.setState({data: data});
+        });
+
+        // Start the interval to do get requests every five seconds
+        this.interval = setInterval(this.tick.bind(this), 5000);
+    }
+
+    tick() {
+        this.getSegment(this.start).then((data) => {
+            var newData = this.state.data.concat(data);
+            this.setState({data: newData});
+            this.start += data.length;
+            if (--this.max === 0) {
+                clearInterval(this.interval);
+                this.interval = null;
+            }
         });
     }
 
     getSegment(start) {
-        return fetch('https://test.encodedcc.org/search/?type=Experiment&format=json')
+        return fetch('https://test.encodedcc.org/search/?type=Experiment&format=json&from=' + start)
         .then(response => {
             return response.text();
         }).then(body => {
             var data = JSON.parse(body);
             return Promise.resolve(data['@graph'].map(result => result.accession));
         });
+    }
+
+    componentWillUnmount() {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
     }
 
     render() {
